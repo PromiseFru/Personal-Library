14e101f56ba639fd70e8036b950bfe377e30ae91
PromiseFru <promisefru1@gmail.com>
2020-11-12 17:50:51 +0100

    get books looking good... now some testing

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,41 @@ module.exports = function (app) {
   app.route('/api/books')
     // TODO - I can get /api/books to retrieve an array of all books containing title, _id, and commentcount.
     // TODO - If I try to request a book that doesn't exist I will be returned 'no book exists'.
-    .get(function (req, res) {
+    .get(async function (req, res) {
+      try {
+        await mongoose.connect(MONGODB_CONNECTION_STRING, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }).then(() => {
+          Book.find({}, (err, books) => {
+            if (err) {
+              console.log(err);
+              return res.json('could not fetch books');
+            }
+
+            var result = [];
+            books.forEach(item => {
+              var comments = item.comments;
+              var count = comments.length;
+
+              var newBook = {
+                _id: item._id,
+                title: item.title,
+                commentcount: count
+              }
+
+              result.push(newBook);
+            })
+
+            res.json(result);
+          })
+        }).catch(err => {
+          console.log(err);
+          return res.json('could not fetch books');
+        })
+      } catch {
+        err => console.log(err)
+      }
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
@@ -83,7 +117,6 @@ module.exports = function (app) {
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
 
-    // TODO - I can post a comment to /api/books/{id} to add a comment to a book and returned will be the books object similar to get /api/books/{id} including the new comment.
     .post(async function (req, res) {
       try {
         var bookid = req.params.id;
@@ -103,7 +136,7 @@ module.exports = function (app) {
             returnOriginal: false,
             useFindAndModify: false
           }, (err, doc) => {
-            if(err) {
+            if (err) {
               console.log(err);
               return res.json('could not create comment')
             }
@@ -116,7 +149,6 @@ module.exports = function (app) {
       } catch {
         err => console.log(err)
       }
-      //json res format same as .get
     })
 
     // TODO - I can delete /api/books/{_id} to delete a book from the collection. Returned will be 'delete successful' if successful.
